e082b8abf80f4408a57cab853885e95dcc8b1446
miingg0322 <miingg0322@gmail.com>
2024-04-24 15:56:53 +0900

    [add] GameManager 추가

---
+++ Assets/01_Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get { return instance; }
+        set { instance = value; }
+    }
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
