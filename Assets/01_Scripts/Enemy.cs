4edaae9eb2631a5c15fb96b9799b52d337489421
miingg0322 <miingg0322@gmail.com>
2024-04-25 11:35:56 +0900

    [add] Enemy 이동 및 공격 틀 추가

--- Assets/01_Scripts/Enemy.cs
+++ Assets/01_Scripts/Enemy.cs
@@ -6,14 +6,46 @@ public class Enemy : MonoBehaviour
 {
     public int hp;
     private int maxHp;
+    public int dmg;
+    public float range = 3f;
+    public float moveSpeed = 2f;
+    public float atkCoolTime = 2f;
+    [SerializeField]
+    private float atkTimer = 0f;
+
+    public SpriteRenderer enemyRend;
+    private Player player;
+
     void Start()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Vector2 curPos = transform.position;
+        Vector2 playerPos = player.transform.position;
+        float distance = Vector2.Distance(curPos, playerPos);
+        if (distance > range)
+        {
+            transform.position = Vector2.MoveTowards(curPos, playerPos, moveSpeed * Time.deltaTime);
+        }
+        else
+        {
+            if (atkTimer <= 0)
+            {
+                NormalAttack();
+            }
+            else
+            {
+                atkTimer -= Time.deltaTime;
+            }
+        }
+    }
+
+    void NormalAttack()
+    {
+        Debug.Log("공격처리");
+        atkTimer = atkCoolTime;
     }
 }
