db167b80e1fa97c5dd5fb7f8b87471f6467e73ba
mhyjw98 <rhapsodymis4@naver.com>
2024-04-25 23:48:44 +0900

    [add] 투척 무기 Script 구현

---
+++ Assets/01_Scripts/Range.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Range : MonoBehaviour
+{
+    public int damage;
+    public float maxDistance;
+
+    bool isTurn = false;
+
+    Rigidbody2D rigid;
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float distance = Vector2.Distance(GameManager.Instance.player.transform.position, rigid.transform.position);
+        transform.Rotate(0,0,Time.deltaTime * 150);
+
+        if(maxDistance < distance)
+        {
+            rigid.velocity *= -1;
+            isTurn = true;
+        }
+    }
+
+    public void Init(Vector3 dir, int dmg)
+    {
+        this.damage = dmg;
+        rigid.velocity = dir * 15f;
+        isTurn = false;
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player") && isTurn)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
