7d211030de069042ac09e3b8329842fbf0d54fd9
mhyjw98 <rhapsodymis4@naver.com>
2024-05-05 06:15:26 +0900

    [add] 캐릭터 삭제 ui 구현,  코드개선

--- Assets/01_Scripts/System/GameManager.cs
+++ Assets/01_Scripts/System/GameManager.cs
@@ -31,6 +31,9 @@ public class GameManager : MonoBehaviour
     public GameObject createBtn;
     public GameObject signupNotice;
     public GameObject slotNotice;
+    public GameObject deleteNotice;
+    public GameObject[] selectBtn;
+    public GameObject[] deleteBtn;
 
     public Image[] characterIcons;
     public Text[] characterTexts;
@@ -38,8 +41,11 @@ public class GameManager : MonoBehaviour
     public string userId;
     public int DungeonLevel;
     public int selectedClass;
+    public int selectIndex;
+    public int deleteCharacter;
 
-    int[] userSlots;  
+    int[] userSlots;
+     
     /// <summary>
     /// Key = Upgrade, Cost, Destroy
     /// </summary>
@@ -107,6 +113,34 @@ public class GameManager : MonoBehaviour
         loginFail.SetActive(false);
     }
 
+    public void ActiveDelete()
+    {
+        for (int index = 0; index < selectBtn.Length; index++)
+        {
+            selectBtn[index].SetActive(false);
+            deleteBtn[index].SetActive(true);
+        }      
+    }
+
+    public void CancleDelete()
+    {
+        for (int index = 0; index < selectBtn.Length; index++)
+        {
+            selectBtn[index].SetActive(true);
+            deleteBtn[index].SetActive(false);
+        }
+    }
+    public void ActiveDeleteCheck(int index)
+    {
+        selectIndex = index;
+        deleteCharacter = userSlots[index];
+        deleteNotice.SetActive(true);           
+    }
+
+    public void CancleDeleteCheck()
+    {
+        deleteNotice.SetActive(false);
+    }
     public void ActiveCreateCharacter()
     {
         if (userSlots[userSlots.Length - 1] != 0)
@@ -148,6 +182,7 @@ public class GameManager : MonoBehaviour
             {
                 characterIcons[index].sprite = null;
                 characterTexts[index].text = "Empty";
+                selectBtn[index].SetActive(false);
             }
             else
             {
@@ -155,21 +190,15 @@ public class GameManager : MonoBehaviour
 
                 characterIcons[index].sprite = playerData[chClass].playerSprite;
                 characterTexts[index].text = playerData[chClass].playerName;
+                selectBtn[index].SetActive(true);
             }
         }
     }
 
     public void SelectCharacter(int slotIndex)
     {
-        if (userSlots != null)
-        {
             selectedClass = DBManager.Instance.GetCharacterClass(userSlots[slotIndex]);
             SceneManager.LoadScene("SampleScene");        
-        }
-        else
-        {
-            Debug.LogError("�����Ͱ� ����ֽ��ϴ�.");
-        }
     }
 
     public void CreateCharacter(int character)
@@ -196,6 +225,16 @@ public class GameManager : MonoBehaviour
         createUi.SetActive(false);
         selectUi.SetActive(true);
         createBtn.SetActive(true);
+    }
+
+    public void DeleteCharacter()
+    {
+        DBManager.Instance.DeleteCharacter(deleteCharacter);
+        deleteNotice.SetActive(false);
+        CancleDelete();
+
+        userSlots[selectIndex] = 0;
+        SetUserSlots(userSlots);
     }
 
     public void AssignPlayer(Player playerRef)
