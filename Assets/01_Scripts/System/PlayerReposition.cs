e4a32c63c3267aee3f3c479ec835042e98bcbaae
mhyjw98 <rhapsodymis4@naver.com>
2024-05-09 15:18:22 +0900

    [fix] 씬 넘어갈때 플레이어 재배치

--- Assets/01_Scripts/System/PlayerReposition.cs
+++ Assets/01_Scripts/System/PlayerReposition.cs
@@ -1,16 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 
 public class PlayerReposition : MonoBehaviour
 {
     void Start()
     {
-        Transform playerTransform = Player.Instance.transform;
+        if (SceneManager.GetActiveScene().name == "AutoFarming")
+        {
+            Transform playerTransform = Player.Instance.transform;
 
-        Vector3 rePosition = new Vector3(7, 0, 0);
-        playerTransform.position = rePosition;
+            Vector3 rePosition = new Vector3(7, 0, 0);
+            playerTransform.position = rePosition;
+        }
+        else
+        {
+            Transform playerTransform = Player.Instance.transform;
+
+            Vector3 rePosition = new Vector3(0, 0, 0);
+            playerTransform.position = rePosition;
+        }
     }
-
 }
