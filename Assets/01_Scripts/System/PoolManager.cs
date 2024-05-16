6ea18bb23e223a6b00a56f9e9aced7333aaa07a6
mhyjw98 <rhapsodymis4@naver.com>
2024-05-16 21:44:09 +0900

    [add] Damage Text 프리팹 추가

--- Assets/01_Scripts/System/PoolManager.cs
+++ Assets/01_Scripts/System/PoolManager.cs
@@ -1,28 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PoolManager : MonoBehaviour
 {
     public GameObject[] prefabs;
-    List<GameObject>[] pools;
+    List<GameObject>[] objPools;
 
     private void Awake()
     {
         GameManager.Instance.AssignPool(this);
-        pools = new List<GameObject>[prefabs.Length];
+        objPools = new List<GameObject>[prefabs.Length];
 
-        for(int index = 0; index < pools.Length; index++)
+        for(int index = 0; index < objPools.Length; index++)
         {
-            pools[index] = new List<GameObject>();
+            objPools[index] = new List<GameObject>();
         }
     }
 
-        public GameObject Get(int index)
+    public GameObject Get(int index)
     {
         GameObject select = null;
 
-        foreach(GameObject pool in pools[index])
+        foreach(GameObject pool in objPools[index])
         {
             if (!pool.activeSelf)
             {
@@ -34,8 +35,19 @@ public class PoolManager : MonoBehaviour
 
         if(select == null)
         {
-            select = Instantiate(prefabs[index],transform);
-            pools[index].Add(select);
+            select = Instantiate(prefabs[index], transform);
+
+            // 4번 pool은 Text라 Canvas에 생성되도록 설정
+            if (index == 4)
+            {
+                GameObject canvasObject = GameObject.Find("Canvas");
+                Canvas canvas = canvasObject.GetComponent<Canvas>();
+
+                select.transform.SetParent(canvas.transform, false);
+            }
+                
+
+            objPools[index].Add(select);
         }
 
         return select;
