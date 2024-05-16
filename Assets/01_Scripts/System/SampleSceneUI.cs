fcdf73441aada848fc930348b0e361e5f0d1bec7
mhyjw98 <rhapsodymis4@naver.com>
2024-05-16 21:44:58 +0900

    [re] 입장권 로직 게임메니저로 이동

--- Assets/01_Scripts/System/SampleSceneUI.cs
+++ Assets/01_Scripts/System/SampleSceneUI.cs
@@ -9,6 +9,12 @@ public class SampleSceneUI : MonoBehaviour
     public Text ticketValue;   
     public Text timer;
 
+    public GameObject[] dungeonEnterBtns;
+
+    private void Awake()
+    {
+        UpdateFarminDungeon();
+    }
     void Update()
     {
         UpdateTimer();
@@ -18,33 +24,50 @@ public class SampleSceneUI : MonoBehaviour
 
     void UpdateTimer()
     {
-        float remainTime = GameManager.Instance.dungeonTicket.ticketGenerationTime - GameManager.Instance.dungeonTicket.timer;
-
-        if (remainTime == GameManager.Instance.dungeonTicket.ticketGenerationTime)
+        if(GameManager.Instance.dungeonTicket == GameManager.Instance.maxDungeonTicket)
         {
-            remainTime = 0f;
+            timer.text = "00:00";
         }
+        else
+        {
+            float remainTime = GameManager.Instance.ticketGenerationTime - GameManager.Instance.timer;
 
-        int min = Mathf.FloorToInt(remainTime / 60);
-        int sec = Mathf.FloorToInt(remainTime % 60);
-        timer.text = string.Format("{0:D2}:{1:D2}", min, sec);
+            if (remainTime == GameManager.Instance.ticketGenerationTime)
+            {
+                remainTime = 0f;
+            }
+
+            int min = Mathf.FloorToInt(remainTime / 60);
+            int sec = Mathf.FloorToInt(remainTime % 60);
+            timer.text = string.Format("{0:D2}:{1:D2}", min, sec);
+        }
     }
 
     void UpdateTicketValue()
     {
-        ticketValue.text = "X " + GameManager.Instance.dungeonTicket.ticket;
+        ticketValue.text = "X " + GameManager.Instance.dungeonTicket;
     }
 
     void UpdateTicketIcon()
     {
-        for (int index = 0; index < GameManager.Instance.dungeonTicket.maxTicket; index++)
+        for (int index = 0; index < GameManager.Instance.maxDungeonTicket; index++)
         {
             ticketIcon[index].color = new Color(1, 1, 1, 0.2f);
         }
 
-        for (int index = 0; index < GameManager.Instance.dungeonTicket.ticket; index++)
+        for (int index = 0; index < GameManager.Instance.dungeonTicket; index++)
         {
             ticketIcon[index].color = new Color(1, 1, 1, 1);
+        }
+    }
+
+    void UpdateFarminDungeon()
+    {
+        int clear = DBManager.Instance.GetBossClear(GameManager.Instance.selectedCharacterId);
+
+        for (int index = 0; index < clear; index++)
+        {
+            dungeonEnterBtns[index].SetActive(true);
         }
     }
 }
