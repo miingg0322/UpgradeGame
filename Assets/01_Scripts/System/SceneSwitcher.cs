aaa3ec5c7cca35ca4635aa56ac1f8acccddbb58c
mhyjw98 <rhapsodymis4@naver.com>
2024-05-16 21:45:54 +0900

    [add] 던전 재도전 기능 추가, 입장권 로직 수정

--- Assets/01_Scripts/System/SceneSwitcher.cs
+++ Assets/01_Scripts/System/SceneSwitcher.cs
@@ -1,26 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Net.Sockets;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using static UnityEngine.Rendering.DebugUI;
 
 public class SceneSwitcher : MonoBehaviour
 {
     int levelValue;
-    int ticketValue;
+    string transUserId;
     public void SwitchAutoFarmingScene(int value)
     {
-        if (GameManager.Instance.dungeonTicket.ticket > 0)
+        if (GameManager.Instance.dungeonTicket > 0)
         {
-            GameManager.Instance.dungeonTicket.ticket--;
-            Debug.Log("던전에 입장하였습니다. 남은 입장권 개수: " + GameManager.Instance.dungeonTicket.ticket);
-            GameManager.Instance.dungeonTicket.SaveData();
+            GameManager.Instance.dungeonTicket--;
+            Debug.Log("던전에 입장하였습니다. 남은 입장권 개수: " + GameManager.Instance.dungeonTicket);
 
             SceneManager.LoadScene("AutoFarming");
 
             SceneManager.sceneLoaded += OnSceneLoaded;
 
             levelValue = value;
+
         }
         else
         {
@@ -33,8 +35,46 @@ public class SceneSwitcher : MonoBehaviour
         SceneManager.LoadScene("SampleScene");
 
         SceneManager.sceneLoaded += OnSceneLoaded;
+    }
 
-        ticketValue = GameManager.Instance.dungeonTicket.ticket;
+    public void GiveUpSwitchSampleScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+
+        GameManager.Instance.Init();
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    public void ReLoadAutoFarmingScene()
+    {
+        if (GameManager.Instance.dungeonTicket > 0)
+        {
+            GameManager.Instance.dungeonTicket--;
+            Debug.Log("던전에 입장하였습니다. 남은 입장권 개수: " + GameManager.Instance.dungeonTicket);
+
+            SceneManager.LoadScene("AutoFarming");
+
+            SceneManager.sceneLoaded += OnSceneLoaded;
+
+            levelValue = GameManager.Instance.DungeonLevel;
+        }
+        else
+        {
+            Debug.Log("던전 입장권이 부족합니다.");
+        }
+    }
+
+    public void ReturnCharacterSelect()
+    {
+        SceneManager.LoadScene("Login");
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+
+        transUserId = GameManager.Instance.userId;
+      
+        GameManager.Instance.ReturnChSelect();
+        Destroy(GameManager.Instance.player.gameObject);
     }
 
     void OnSceneLoaded(Scene scene, LoadSceneMode mode)
@@ -50,22 +90,31 @@ public class SceneSwitcher : MonoBehaviour
             {
                 Debug.LogError("GameManager를 찾을 수 없습니다.");
             }
-
-            SceneManager.sceneLoaded -= OnSceneLoaded;
         }
         else if(scene.name == "SampleScene")
         {
             GameManager gameManager = FindObjectOfType<GameManager>();
             if (gameManager != null)
             {
-                gameManager.dungeonTicket.ticket = ticketValue;
-                Time.timeScale = 1f;
-                GameManager.Instance.dungeonTicket.Init();
+                Time.timeScale = 1f;              
             }
             else
             {
                 Debug.LogError("GameManager를 찾을 수 없습니다.");
             }
         }
+        else if (scene.name == "Login")
+        {
+            GameManager gameManager = FindObjectOfType<GameManager>();
+            if (gameManager != null)
+            {
+                gameManager.userId = transUserId;
+            }
+            else
+            {
+                Debug.LogError("GameManager를 찾을 수 없습니다.");
+            }
+        }
+        SceneManager.sceneLoaded -= OnSceneLoaded;
     }
 }
