f905c8d5e09acc7b6cb7d2b43a53119cdb3b41d5
mhyjw98 <rhapsodymis4@naver.com>
2024-05-16 21:36:32 +0900

    [add] 건슬링거 파밍맵 공격 구현

---
+++ Assets/01_Scripts/Weapon/Explosion.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public float damage = 250f;
+
+    float timer = 0f;
+    private void OnEnable()
+    {
+        transform.localScale = Vector3.zero;
+        timer = 0f;
+    }
+    void Update()
+    {
+        if (timer < 0.3f)
+        {
+            timer += Time.deltaTime;
+            transform.localScale += Vector3.one * 5f * Time.deltaTime;
+        }
+        else
+        {
+            timer = 0;           
+            gameObject.SetActive(false);          
+        }        
+    }
+}
