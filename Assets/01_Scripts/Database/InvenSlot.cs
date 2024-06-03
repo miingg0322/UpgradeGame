62190b822383a3977d8784b3939c34b360611190
miingg0322 <miingg0322@gmail.com>
2024-06-03 15:05:58 +0900

    [add] 무기 등록 슬롯 추가

--- Assets/01_Scripts/Database/InvenSlot.cs
+++ Assets/01_Scripts/Database/InvenSlot.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class InvenSlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+public class InvenSlot : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
     public FollowDetail followDetail;
     public Item slotItem;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if(eventData.pointerId == -2)
+        {
+            WeaponSlot slot = FindAnyObjectByType<WeaponSlot>();
+            if (slot && slotItem.type.Equals(0))
+            {
+                slot.RegisterItemIntoSlot(slotItem);
+            }
+        }
+    }
+
     public void OnPointerEnter(PointerEventData eventData)
     {
         followDetail.IsVisible = true;
